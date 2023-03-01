c9f168bf4a741e1ef6707235e25ef91320674337
jmfp <therealjmfp@gmail.com>
2023-03-01 10:36:11 -0500

    commit for hosting

--- PikaPlay/frontend/src/components/Header.js
+++ PikaPlay/frontend/src/components/Header.js
@@ -6,6 +6,7 @@ export default function Header() {
     <header className='header-invis'>
         <div className='logo-container'>
             <Link to='/'>Home</Link>
+            <Link to='/query'>query</Link>
         </div>
     </header>
   )
